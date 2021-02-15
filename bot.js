e29e05a534158458b3a6369e92ebf50ba2dbde5c
paestells <77740351+paestells@users.noreply.github.com>
2021-02-14 23:50:46 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('Logged in as ${client.user.tag}?');
-    client.user.setActivity('volleyball games with my baby', { type: 'WATCHING' });
+    client.user.setActivity('volleyball games with Taylor', { type: 'WATCHING' });
 });
 
 
